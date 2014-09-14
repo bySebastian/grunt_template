2ee1f9f01131926f5de8328c26614df3eeb66ef9
bySebastian <creativity@madebysebastian.be>
2014-09-14 22:27:42 +0200

    updating README.md

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,48 +8,141 @@ module.exports = function(grunt){
 		pkg:grunt.file.reasJSON('package.json'),
 
 		// == CONFIG ==
+		clean: {
+			build: [
+				'build'
+			]
+		},
+
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'assets/sass',
+					cssDir: 'build/assets/css',
+					environment: 'production',
+					// require: 'zurb-foundation'
+				}
+			},
+			dev: {
+				options: {
+					sassDir: 'assets/sass',
+					cssDir: 'build/assets/css',
+					envirionment: 'development',
+					// require: 'zurb-foundation'
+				}
+			}
+		},
+
+		jshint: {
+			work: [
+				'assets/js/*.js',
+				'Gruntfile.js'
+			]
+		},
+
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 3
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'build/',
+						src: ['**/*.jpg'],
+						dest: 'build/',
+						ext: '.jpg'
+					},
+					{
+						expand: true,
+						cwd: 'build/',
+						src: ['**/*.png'],
+						dest: 'build/',
+						ext: '.png'
+					}
+				]
+			}
+		},
+
+		uglify: {
+			dist: {
+				files: {
+					'build/assets/js/app.min.js': [
+						'assets/js/vender/jquery.js',
+						'assets/js/app.js'
+					]
+				}
+			}
+		},
+
+		cssmin: {
+			dist: {
+				expand: true,
+				cwd: 'assets/css',
+				src: ['*.css'],
+				dest: 'build/assets/css'
+			}
+		}
+
 		watch:{
 			options: {
 				lifereload: true
 			}
 
 			compass: {
-				files: ['**/*.{scss,sass}'],
+				files: ['assets/sass/**/*.{scss,sass}'],
 				tasks: ['compass:dev']
 			},
 
 			js: {
-				files: ['js/**/*.js'],
-				tasks: ['uglify']
+				files: ['assets/js/*.js'],
+				tasks: ['jshint:work','uglify']
 			}
 		},
 
-		compass: {
-			dev: {
+		sshconfig: {
+			dist: {
+				host: 'someserver.com',
+				username: 'someuser',
+				agent: process.env.SSH_AUTH_SOCK
+			},
+
+			staging: {
+				host: 'someserver.com',
+				username: 'someuser',
+				agent: process.env.SSH_AUTH_SOCK
+			},
+		}
+
+		sshexec: {
+			staging: {
+				command: [
+					'cd /home/someuser/app',
+					'git pull origin master'
+					'npm install'
+					'forever stop server.js'
+					'forever start server.js'
+					'forever list'
+				].join('&&'),
 				options: {
-					sassDir: ['styles/sass'],
-					cssDir: ['styles/css'],
-					environment: 'development'
+					config: 'staging'
 				}
 			},
-			prod: {
-				options: {
-					sassDir: ['styles/sass'],
-					cssDir: ['styles/css'],
-					environment: 'production'
-				}
-			}
-		},
 
-		uglify: {
-			all: {
-				files: {
-					'js/min/main.min.js': [
-						'js/libs/jquery.js',
-						'js/main.js'
-					]
+			dist: {
+				command: [
+					'cd /home/someuser/app',
+					'git pull origin master'
+					'npm install'
+					'forever stop server.js'
+					'forever start server.js'
+					'forever list'
+				].join('&&'),
+				options: {
+					config: 'dist'
 				}
 			}
+			
 		}
 
 		express:{
@@ -67,16 +160,18 @@ module.exports = function(grunt){
 
 	/*
 	// == DEPENDENT PLUGINS == 
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTesks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-express');
 	grunt.loadNpmTasks('grunt-ftp-deploy');
 	*/
 
 	// == TASKS ==
-	grunt.registerTask('default',['compass:dev','uglify','express','watch']);
-	grunt.registerTask('staging',['compass:dev','uglify']);
-	grunt.registerTask('production',['compass:prod','uglify']);
+	grunt.registerTask('dev',['watch']);
+	grunt.registerTask('staging',['clean:build','compass:dist','sshexec:staging']);
+	grunt.registerTask('production',['clean:build','compass:dist','cssmin:dist','uglify:dist','sshexec:dist']);
 
 }
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-My first grunt template
-=======================
+
 
 ## 1. Preparing ##
 
@@ -9,16 +8,24 @@ My first grunt template
 
 ## 2. Project ##
 
-* mkdir template
-* cd template
+* mkdir <yourProjectDir>
+* cd <yourProjectDir>
 * npm init
 * new-item readme.MD
 * npm install grunt --save-dev 
-* npm install grunt-contrib-watch --save-dev
+* npm install grunt-contrib-clean --save-dev
 * npm install grunt-contrib-compass --save-dev // requires ruby, sass, compass
+* npm install grunt-contrib-jshint --save-dev
+* npm install grunt-contrib-imagemin --save-dev
+* npm install grunt-contrib-watch --save-dev
 * npm install grunt-contrib-uglify --save-dev
+* npm install grunt-contrib-cssmin --save-dev
+* npm install grunt-shell --save-dev
 * npm install grunt-ftp-deploy --save-dev
 * npm install grunt-express --save-dev
 * ...
 * mkdir sass ... // making folder structure
 * new-item Gruntfile.js // making and editing Grundfile.js
+
+
+### Tip: add Gruntfile.js to .gitignore ###
